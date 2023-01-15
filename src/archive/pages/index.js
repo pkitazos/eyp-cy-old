b01610da87fc3a01d1eb9f142925f172426d8bfe
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-01-15 01:26:20 +0000

    removed archive files

--- src/archive/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as AboutUs } from "./AboutUs";
-export { default as Impact } from "./Impact";
-export { default as Patrons } from "./Patrons";
-export { default as Events } from "./Events";
-export { default as GetInvolved } from "./GetInvolved";
