b03c2ce5aed207a5c2781cc60d38e953c7b750a5
Petros Kitazos <petroskitazos@gmail.com>
2023-04-08 17:11:40 +0100

    minor UI changes to Upcoming Events card

--- src/components/Cards/UpcomingEventCard.tsx
+++ src/components/Cards/UpcomingEventCard.tsx
@@ -21,14 +21,14 @@ const UpcomingEventCard: FC<props> = ({
       <div className='group h-full w-60 max-w-xs overflow-hidden rounded-2xl bg-slate-100 shadow-lg transition-all duration-300 hover:scale-[1.02]'>
         <img src={imageURL} className='h-2/5 w-full object-cover' />
         <div className='h-3/5 p-6'>
-          <h3 className='w-max text-xl font-semibold text-primary-800'>
+          <h3 className='w-max text-xl font-semibold text-primary-800 transition-all duration-200 group-hover:font-bold group-hover:text-primary-700'>
             {title}
           </h3>
-          <h4 className='text-blue-600'>{date}</h4>
-          <div className='mb-4 mt-3 h-0.5 w-16 bg-primary-700 group-hover:bg-orange-600'>
-            &nbsp;
-          </div>
-          <p className='pb-6 leading-normal'>{description}</p>
+          <h4 className=' underline decoration-secondary-900 decoration-wavy underline-offset-8 transition-all duration-200 group-hover:font-bold group-hover:decoration-primary-700'>
+            {date}
+          </h4>
+
+          <p className='mt-3 pb-6 leading-normal'>{description}</p>
         </div>
       </div>
     </a>
