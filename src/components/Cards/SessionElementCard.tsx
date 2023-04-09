b0794db104ffa048f39df057d5ed788ac5805f2a
Petros Kitazos <petroskitazos@gmail.com>
2023-04-09 15:53:22 +0100

    updated TODOs

--- src/components/Cards/SessionElementCard.tsx
+++ src/components/Cards/SessionElementCard.tsx
@@ -4,7 +4,7 @@ interface props {
   title: string;
   titlePosition?: "text-left" | "text-center" | "text-right";
   children: ReactNode;
-  image_URL?: string; // todo not yet used
+  imageURL?: string; // TODO not yet used
   className?: string;
 }
 
@@ -12,7 +12,7 @@ const SessionElementCard: FC<props> = ({
   title,
   titlePosition = "text-left",
   children,
-  image_URL,
+  imageURL,
   className,
 }) => {
   return (
@@ -21,15 +21,15 @@ const SessionElementCard: FC<props> = ({
         className={`${className} flex h-max flex-row items-center gap-5 
         rounded-3xl bg-white shadow-sm transition-all duration-300 hover:scale-[1.02]`}
       >
-        {/* <img src={image_URL} className='' /> */}
+        {/* <img src={imageURL} className='' /> */}
 
-        <div className="z-10 m-5 sm:m-6  md:m-8">
+        <div className='z-10 m-5 sm:m-6  md:m-8'>
           <h2
             className={`mb-4 ${titlePosition} text-xl font-bold underline decoration-secondary-900 decoration-wavy underline-offset-2 xs:text-2xl xs:font-semibold xl:text-3xl`}
           >
             {title}
           </h2>
-          <p className="text-justify text-lg">{children}</p>
+          <p className='text-justify text-lg'>{children}</p>
         </div>
       </div>
     </>
