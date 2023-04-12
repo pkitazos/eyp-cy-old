145052be43a6982295c5f1d5b4a7edfcd9a62ac0
Petros Kitazos <petroskitazos@gmail.com>
2023-04-12 12:49:50 +0100

    made extractStuff script tograb the color and paths from the custom SVGs

---
+++ src/utils/extractStuff.py
@@ -0,0 +1,35 @@
+import numpy as np
+from xml.dom import minidom
+
+# (?<=d=)".*"(?= )
+
+
+def toCamelCase(str):
+    lst = str.split("-")
+    lst = [lst[0]] + [word.capitalize() for word in lst[1:]]
+    return "".join(lst)
+
+
+def extractStuff(filename):
+    frames = []
+    for i in range(1, 6):
+        doc = minidom.parse(f"./src/assets/{filename}-{i}.svg")
+        path_strings = [path.getAttribute("d") for path in doc.getElementsByTagName("path")]
+        doc.unlink()
+        frames.append(path_strings)
+
+    layers = np.array(frames).T.tolist()
+
+    doc = minidom.parse(f"./src/assets/{filename}-1.svg")
+    colors = [path.getAttribute("fill") for path in doc.getElementsByTagName("path")]
+    doc.unlink()
+
+    paths = {}
+    paths[toCamelCase(filename)] = {}
+    paths[toCamelCase(filename)]["layers"] = layers
+    paths[toCamelCase(filename)]["colors"] = colors
+    return paths
+
+
+print("const paths = ", end="")
+print(extractStuff("header-diagonal"))
