c6afb09e99d61b934e4837c0af959dea015de0d8
Petros Kitazos <petroskitazos@gmail.com>
2023-04-14 21:17:29 +0100

    added on-scroll animations to impact, national-committee, and patrons-and-partners pages

--- src/components/Cards/ImpactCard.tsx
+++ src/components/Cards/ImpactCard.tsx
@@ -16,16 +16,14 @@ const ImpactCard: FC<props> = ({
   children,
   underlineColor = "text-accent-900",
 }) => {
+  let rowDirection = reversed ? "sm:flex-row-reverse" : "sm:flex-row";
+  let hideDirection = reversed ? "hide-left" : "hide-right";
   return (
-    <div
-      className={`flex ${
-        reversed ? "sm:flex-row-reverse" : "sm:flex-row"
-      } items-center gap-10`}
-    >
-      <div className='hidden h-full sm:flex lg:w-1/2 xl:w-2/5'>
+    <div className={`flex ${rowDirection} items-center gap-10`}>
+      <div className={`${hideDirection} delay-card hidden h-full sm:flex lg:w-1/2 xl:w-2/5`}>
         <MaskedImage imageURL={imageURL} />
       </div>
-      <div className=' flex flex-col justify-center gap-y-4 lg:w-1/2 xl:w-3/5'>
+      <div className={`${hideDirection} flex flex-col justify-center gap-y-4 lg:w-1/2 xl:w-3/5`}>
         <Title text={title} order='h2' underlineColor={underlineColor} />
         <p className='text-justify text-lg leading-relaxed md:text-xl'>
           {children}
