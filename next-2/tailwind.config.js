4fcbc68fcc8f24ff02742707fa32d5bc8f84fee3
pkitazos <petroskitazos@gmail.com>
2023-06-30 15:54:10 +0100

    extended utility classes

--- next-2/tailwind.config.js
+++ next-2/tailwind.config.js
@@ -24,16 +24,28 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+        9: "repeat(9, minmax(0, 1fr))",
+        10: "repeat(10, minmax(0, 1fr))",
+        11: "repeat(11, minmax(0, 1fr))",
+        12: "repeat(12, minmax(0, 1fr))",
+      },
       gridRow: {
         "span-8": "span 8 / span 8",
+        "span-9": "span 9 / span 9",
+        "span-10": "span 10 / span 10",
+        "span-11": "span 11 / span 11",
+        "span-12": "span 12 / span 12",
       },
       gridRowStart: {
+        7: "7",
         8: "8",
         9: "9",
         10: "10",
         11: "11",
         12: "12",
-        13: "13",
       },
       borderRadius: {
         "4xl": "2rem",
