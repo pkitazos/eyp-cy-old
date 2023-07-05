e1f32c1ff786b6e7e4839908bf1a27bc9bbb26d9
pkitazos <petroskitazos@gmail.com>
2023-07-05 18:03:13 +0100

    added Impact page

---
+++ next-2/src/components/ImpactCard.tsx
@@ -0,0 +1,47 @@
+import type { FC } from "react";
+import { Title } from "~/components";
+import Image from "next/image";
+
+interface props {
+  title: string;
+  imageURL: string;
+  children: React.ReactNode;
+  reversed?: boolean;
+  underlineColor?: string;
+}
+
+const ImpactCard: FC<props> = ({
+  title,
+  imageURL,
+  reversed,
+  children,
+  underlineColor = "text-accent-900",
+}) => {
+  let rowDirection = reversed ? "sm:flex-row-reverse" : "sm:flex-row";
+  let hideDirection = reversed ? "hide-left" : "hide-right";
+  return (
+    <div className={`flex ${rowDirection} items-center gap-10`}>
+      <div
+        className={`${hideDirection} delay-card hidden h-full sm:flex lg:w-1/2 xl:w-2/5`}
+      >
+        <Image
+          src={imageURL}
+          alt={""}
+          className="masked-image"
+          width={300}
+          height={300}
+        />
+      </div>
+      <div
+        className={`${hideDirection} flex flex-col justify-center gap-y-4 lg:w-1/2 xl:w-3/5`}
+      >
+        <Title text={title} order="h2" underlineColor={underlineColor} />
+        <p className="text-justify text-lg leading-relaxed md:text-xl">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ImpactCard;
