9c70633baeb4dcf7ac768a0174a7ddf76ab3bc3b
pkitazos <petroskitazos@gmail.com>
2023-09-11 13:07:08 +0100

    fixed bottom padding

--- src/app/about-us/national-committee/page.tsx
+++ src/app/about-us/national-committee/page.tsx
@@ -80,7 +80,7 @@ export default function NationalCommittee() {
             </div>
           </div>
         </section>
-        <section className="page-margin mt-16 flex flex-col justify-center gap-2">
+        <section className="page-margin mt-16 mb-20 flex flex-col justify-center gap-2">
           <div className="hide-left flex justify-center">
             <Title
               text="Previous Boards"
--- src/components/previous-board-button.tsx
+++ src/components/previous-board-button.tsx
@@ -18,7 +18,7 @@ export function PreviousBoardButton({ year, boardMembers }: props) {
             {year}
           </button>
         </DialogTrigger>
-        <DialogContent className="backdrop-blur-xl">
+        <DialogContent className="bg-white outline-none backdrop-blur-xl">
           <DialogHeader>
             <Title text={`National Committee ${year}`} order="h2" />
           </DialogHeader>
