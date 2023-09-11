2ff053e310b087c110106b76d75e539f75c8e8a4
pkitazos <petroskitazos@gmail.com>
2023-09-11 16:33:02 +0100

    #45

--- src/components/SVGs/CloseIcon.tsx
+++ src/components/SVGs/CloseIcon.tsx
@@ -1,17 +1,25 @@
 import type { FC } from "react";
 
 interface props {
-  className: string;
+  className?: string;
 }
 
 const CloseIcon: FC<props> = ({ className }) => {
   return (
     <div className={className}>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'>
-        <path
-          d='M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z'
-          fill='currentColor'
-        />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
       </svg>
     </div>
   );
