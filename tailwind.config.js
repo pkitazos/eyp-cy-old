5697aa2b5a81833254607ac2d251fb50f6a7af42
pkitazos <petroskitazos@gmail.com>
2023-09-16 13:11:59 +0100

    updated tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+const plugin = require("tailwindcss/plugin");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -61,10 +66,17 @@ module.exports = {
           900: "#00B7FF",
         },
         accent: {
+          700: "#FFE876",
           800: "#FFDE4D",
           900: "#FFD200",
         },
         "base-white": "#f9f9f9",
+      },
+      backgroundImage: {
+        "underliner-secondary":
+          "linear-gradient(transparent calc(100% - 10px), #33C5FF 5px);",
+        "underliner-orange":
+          "linear-gradient(transparent calc(100% - 10px), orange 5px);",
       },
       fontFamily: {
         sans: ["var(--font-source-sans-3)"],
@@ -85,5 +97,91 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, matchUtilities, theme }) {
+      matchUtilities(
+        {
+          dbg: (value) => ({
+            outline: "2px solid",
+            outlineColor: value,
+          }),
+        },
+        { values: flattenColorPalette(theme("color")), type: "color" }
+      ),
+        addUtilities({
+          ".diamond-mask": {
+            maskImage: "url('/diamond-mask.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+            maskPosition: "center",
+          },
+          // debug classes
+          ".--orange-500": {
+            outline: "2px solid",
+            outlineColor: "#f97316",
+          },
+          ".--orange-600": {
+            outline: "2px solid",
+            outlineColor: "#ea580c",
+          },
+
+          ".--amber-500": {
+            outline: "2px solid",
+            outlineColor: "#f59e0b",
+          },
+          ".--amber-600": {
+            outline: "2px solid",
+            outlineColor: "#d97706",
+          },
+          ".--lime-500": {
+            outline: "2px solid",
+            outlineColor: "#84cc16",
+          },
+          ".--lime-600": {
+            outline: "2px solid",
+            outlineColor: "#65a30d",
+          },
+          ".--emerald-500": {
+            outline: "2px solid",
+            outlineColor: "#10b981",
+          },
+          ".--emerald-600": {
+            outline: "2px solid",
+            outlineColor: "#059669",
+          },
+          ".--sky-500": {
+            outline: "2px solid",
+            outlineColor: "#0ea5e9",
+          },
+          ".--sky-600": {
+            outline: "2px solid",
+            outlineColor: "#0284c7",
+          },
+          ".--blue-500": {
+            outline: "2px solid",
+            outlineColor: "#3b82f6",
+          },
+          ".--blue-600": {
+            outline: "2px solid",
+            outlineColor: "#2563eb",
+          },
+          ".--purple-500": {
+            outline: "2px solid",
+            outlineColor: "#a855f7",
+          },
+          ".--purple-600": {
+            outline: "2px solid",
+            outlineColor: "#9333ea",
+          },
+          ".--pink-500": {
+            outline: "2px solid",
+            outlineColor: "#ec4899",
+          },
+          ".--pink-600": {
+            outline: "2px solid",
+            outlineColor: "#db2777",
+          },
+        });
+    }),
+  ],
 };
