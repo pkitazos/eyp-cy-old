3b152a24de6065b96e27e34f2c9ab5f43d892d91
pkitazos <petroskitazos@gmail.com>
2023-09-16 15:48:05 +0100

    updated assets index

--- src/assets/home/index.ts
+++ src/assets/home/index.ts
@@ -1,5 +1,5 @@
-import dWhoWeAre1 from "./d-22.webp";
-import dWhoWeAre3 from "./d-44.webp";
-import dWhoWeAre2 from "./d-46.webp";
+import dWhoWeAre1 from "./d-who-we-are-1.webp";
+import dWhoWeAre2 from "./d-who-we-are-2.webp";
+import dWhoWeAre3 from "./d-who-we-are-3.webp";
 
 export { dWhoWeAre1, dWhoWeAre2, dWhoWeAre3 };
